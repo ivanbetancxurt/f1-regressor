0cdfd2b80113462845d61c66fe056ede83769ac4
Ivan Betancourt <ivanab252@gmail.com>
2025-04-16 22:56:19 +0200

    Add preprocessing script

--- generate_data.py
+++ generate_data.py
@@ -1,6 +1,5 @@
 import os
 import pandas as pd
-import numpy as np
 import requests as req
 import json as JSON
 
---
+++ preprocess.py
@@ -0,0 +1,32 @@
+import pandas as pd
+
+def fastest_lap_to_seconds(df): # convert 'mm:ss.ss' format to seconds
+    def convert(time): # define conversion funciton
+        if time != '300':
+            try:
+                minutes, seconds = time.split(':')
+                return (float(minutes) * 60) + float(seconds)
+            except Exception as e:
+                print(time)
+        else:
+            return 300
+
+    df['fastest_lap'] = df['fastest_lap'].apply(convert) # apply
+
+    return df
+
+def one_hot_encode(df): # one-hot encode categorical columns
+    df = pd.get_dummies(df, columns=['circuit_id', 'driver_id', 'constructor_id', 'status'], prefix={ # encode
+        'circuit_id': 'circuit',
+        'driver_id': 'driver',
+        'constructor_id': 'constructor',
+        'status': 'status'
+    })
+
+    # convert values encoded to True and False to 1 and 0
+    bool_cols = df.select_dtypes(include=['bool']).columns 
+    df[bool_cols] = df[bool_cols].astype(int)
+
+    df.to_csv('data/dataframes/raw_numerical.csv', index=False) # save
+
+    return df
